b1b91c1b76062bba48062b84efc9a469568b5c20
Nathan Golba <ngolba@gmail.com>
2018-09-22 20:09:45 -0400

    Random Functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,13 +8,9 @@ let yourMood = {};
 let movieList = [];
 let movieTitles = [];
 let carouselLoaded = false;
-let player0 = {};
-let player1 = {};
-let player2 = {};
-let player3 = {};
-let player4 = {};
-let player5 = {};
-let players = [player0, player1, player2, player3, player4, player5]
+let players = [{}, {}, {}, {}, {}, {}]
+// let players = [player0, player1, player2, player3, player4, player5]
+let allGenres = [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37]
 let youtubeAPIReady = false;
 let moodWord = '';
 
@@ -32,8 +28,9 @@ const mad = new MoodProfile('mad', 2, [28, 53, 10752], ['cruel', 'angry', 'ruin'
 const lonely = new MoodProfile('lonely', 3, [99, 878, 10749], ['dystopian', 'heartbroken', 'personal', 'lonely', 'loss', 'heartbreak', 'seperation', 'break-up', 'abandon', 'alone', 'love'])
 const inLove = new MoodProfile('inLove', 4, [12, 35, 10749], ['magical', 'charming', 'love', 'romance', 'romantic', 'wedding', 'marriage', 'boyfriend', 'girlfriend'])
 const silly = new MoodProfile('silly', 5, [35, 14, 10402], ['exciting', 'outrageous', 'laughter', 'satire', 'comedy', 'wild', 'high jinks', 'mockumentary', 'funny', 'laughter', 'party', 'scheme'])
+const random = new MoodProfile('random', 6, _.sample(allGenres, 3), null)
 
-const moodObjectArray = [happy, sad, mad, lonely, inLove, silly]
+const moodObjectArray = [happy, sad, mad, lonely, inLove, silly, random]
 const moodStringArray = moodObjectArray.map(x => x.english)
 
 
@@ -77,7 +74,8 @@ const carouselFiller = (index, movieList) => {
                 videoId: '',
                 playerVars: {
                     listType: 'search',
-                    list: ''
+                    list: '',
+                    suggestedQuality: 'large'
                 },
                 events: {
                     'onReady': onPlayerReady
@@ -101,19 +99,16 @@ const cardGenerator = (movieList) => {
     $('#cardsGoHere').empty();
     $('#cardsGoHere').append('<div class="container" id="cardContainer">')
     $('#cardContainer').append('<div class="card-deck" id="movieCards1">')
-    for (let cardIndex = 0; cardIndex < movieList.length; cardIndex++) {        let card = $(`<div class="card" id="card${cardIndex}">`)
+    for (let cardIndex = 0; cardIndex < movieList.length; cardIndex++) {
+        let card = $(`<div class="card" id="card${cardIndex}">`)
         let cardTop = $(`<div id="moodMovie${cardIndex}">` + `<img class="card-img-top" src="http://image.tmdb.org/t/p/w185/${movieList[cardIndex].poster_path}">`)
         let cardBody = $(`<div class="card-body">`)
         let cardTitle = $(`<h5 class="card-text">${movieList[cardIndex].title}</h5><p class="card-text">${movieList[cardIndex].release_date.substring(0, 4)}</p>`)
-
         cardBody.append(cardTitle)
         $('#movieCards1').append(card)
-        if (cardIndex === 3) {
-            $('#card3').addClass('d-md-block')
-        }
         $(`#card${cardIndex}`).append(cardTop, cardBody)
-        $(`#card${cardIndex}`).click((event) => {
-            $('.card-img-top').slideUp(300)
+        $(`#card${cardIndex}`).click( () => {
+            $('.card-img-top').hide()
             $('#carouselContainer').show();
             carouselFiller(cardIndex, movieList);
         })
@@ -131,7 +126,6 @@ const checkPlots = (allMovies, plots, mood) => {
             }
         }
         if (plotIndex === plots.length - 1) {
-            console.log(_.uniq(allApplicableMovies))
             movieList = _.sample(_.uniq(allApplicableMovies), 6)
             cardGenerator(movieList);
         }
@@ -144,17 +138,23 @@ const manyRandomMovies = (yourMood) => {
     let arrayOfMovieArrays = [];
     let allMovies = [];
     let allPlots = [];
-    for (let page = 1; page <= 50; page++) {
+    for (let page = 1; page <= 40; page++) {
         let moodQueryUrl = `https://api.themoviedb.org/3/discover/movie?with_original_language=en&with_genres=${yourMood.genres[0]}|${yourMood.genres[1]}|${yourMood.genres[2]}&page=${page}&include_adult=false&language=en-US&api_key=${tmdbAPIkey}`;
         $.get(moodQueryUrl).then((response) => {
+            arrayOfMovieArrays.push(response.results)
+            if (arrayOfMovieArrays.length === 40) {
+                allMovies = _.flatten(arrayOfMovieArrays);
+             ////// Skips plots for random button ////////
+                if (!yourMood.plotWords) {             
+                    movieList = _.sample(_.uniq(allMovies), 6);
+                    cardGenerator(movieList);
+                } else {
+                    allPlots = _.pluck(allMovies, 'overview');
+                    checkPlots(allMovies, allPlots, yourMood);
+                }
+            }
             if (!response) {
                 console.log('error')
-            }
-            arrayOfMovieArrays.push(response.results)
-            if (arrayOfMovieArrays.length === 50) {
-                allMovies = _.flatten(arrayOfMovieArrays);
-                allPlots = _.pluck(allMovies, 'overview');
-                checkPlots(allMovies, allPlots, yourMood)
             }
         })
     }
@@ -170,9 +170,7 @@ $('.moodButtons').click(function () {
     moodWord = $(this).attr('mood')
     moodIndex = moodStringArray.indexOf(moodWord);
     let yourMood = moodObjectArray[moodIndex]
-    console.log(yourMood)
     manyRandomMovies(yourMood);
     $('.cover-heading').hide()
     $('.mastheadAfterMood').show()
 })
-
